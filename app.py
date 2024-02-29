d608d81e04528d08d4d8262dfc89de5c0ac7d57d
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-29 20:42:47 -0300

    a

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ bar_chart.layout.yaxis.fixedrange = True
 
 pie_chart1 = px.pie(dfpie, names="Valorização", values='count',title="Você sente-se valorizado pelo seu trabalho?")
 
-pie_chart_valor = px.pie(dfpie_valor,names=["Sim", "Não"],values='Conexão com Colegas',title='Sentimento de Conexão com Colegas')
+pie_chart_valor = px.pie(dfpie_valor,names='Conexão com Colegas',values='count',title='Sentimento de Conexão com Colegas')
 
 
 #-----------------------------------------------------------------------------------------------------------------------------
