1818bf27d4d53b39bd42278544b5907465883789
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-29 20:32:28 -0300

    a

--- app.py
+++ app.py
@@ -118,10 +118,12 @@ categorias = {'Muito bom': 'Muito bom','Ótimo': 'Ótimo','Bom': 'Bom','Ruim': '
 
 
 
-# pie_chart1 = px.pie(dfpie, names=["Sim", "Não"], values='Valorização',title="Você sente-se valorizado pelo seu trabalho?")
+pie_chart1 = px.pie(dfpie, names=["Sim", "Não"], values='Valorização',title="Você sente-se valorizado pelo seu trabalho?")
 
-# pie_chart_valor = px.pie(dfpie_valor,names=["Sim", "Não"],values='Conexão com Colegas',title='Sentimento de Conexão com Colegas')
+pie_chart_valor = px.pie(dfpie_valor,names=["Sim", "Não"],values='Conexão com Colegas',title='Sentimento de Conexão com Colegas')
 
+with col1:
+    st.plotly_chart(pie_chart1)
 
 # #-----------------------------------------------------------------------------------------------------------------------------
 
