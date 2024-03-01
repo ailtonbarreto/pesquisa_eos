1454812f915b9797bd03deca7c1645d304ecfb73
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-29 22:23:51 -0300

    updated

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import plotly_express as px
 st.set_page_config(page_title="Pesquisa de Satifação",layout="wide")
 
 #layout
-
-cola, = st.columns(1)
+st.title("Pesquisa de Satisfação Janeiro 2024", anchor= False)
+st.divider()
 col1,col2,col3,col4, col5 = st.columns(5)
 col6,col7 = st.columns(2)
 col8, col9 = st.columns(2)
@@ -19,8 +19,6 @@ col10, = st.columns(1)
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html = True)
 
-with cola:
-    st.title("Pesquisa de Satisfação Janeiro 2024", anchor= False)
 
 
 #-----------------------------------------------------------------------------------------------------------------------------
