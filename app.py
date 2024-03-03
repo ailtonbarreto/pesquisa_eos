6539159b15b8b9382287f18ae9809a524af5c63d
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-03-02 23:57:08 -0300

    a

--- app.py
+++ app.py
@@ -192,7 +192,8 @@ with col10:
     st.plotly_chart(bar_char_avaliacao,use_container_width=True)   
 
 st.divider()
-st.subheader('Acessar Pesquisa',anchor=False)
+# st.subheader('Acessar Pesquisa',anchor=False)
+st.expander("Acessar Pesquisa",expanded=False)
 st.image("link.png",width=300)
 st.link_button("Acessar",url ='https://docs.google.com/forms/d/e/1FAIpQLSeyzFMc7bFvPgmHreIAOhIWOB9PugK7NfAIpbEr6ReXJORfjg/viewform?usp=sf_link')
     
