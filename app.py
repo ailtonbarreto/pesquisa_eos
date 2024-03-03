1ea628f45c1b64493c259baabdf2254ab873455e
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-03-03 01:23:11 -0300

    a

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ df['Categoria'] = df['Como você avalia o seu Gestor?'].map(categorias)
 
 contagem_categorias = df['Categoria'].value_counts()
 
-contagem_categorias = contagem_categorias.map(valor_order)
+contagem_categorias = contagem_categorias.map(categorias)
 
 contagem_categorias = df['Categoria'].value_counts().sort_values(ascending=False)
 #-----------------------------------------------------------------------------------------------------------------------------
