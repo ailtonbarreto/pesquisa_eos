eb863dc066df9c5a9cb6be8d28e556732492918c
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-03-03 00:41:40 -0300

    a

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ df['Categoria'] = df['Como você avalia o seu Gestor?'].map(categorias)
 
 
 contagem_categorias = df['Categoria'].value_counts().sort_values(ascending=False)
-contagem_categorias - contagem_categorias.sort_values("Count",ascending=False)
+contagem_categorias - contagem_categorias.sort_values(ascending=False)
 
 df_count = len(df)
 count_nao = (df['Valorização'] == 'Sim').sum()
