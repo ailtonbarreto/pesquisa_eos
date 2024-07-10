35db9e4bd45ca24ade1540d6efb799cab81b35ce
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-07-09 21:40:44 -0300

    a

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ borderselect = """
     [data-testid="column"]
     {
     padding: 15px;
-    background-color: #003459;
+    background-color: #001d3d;
     border-radius: 12px;
     opacity: 97%;
     text-align: center;
